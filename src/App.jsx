ab17ad016345282b9610aae6082376ccd6d7d58c
NM <nanayamiki@gmail.com>
2022-11-09 12:04:26 -0800

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,40 @@
-import React from "react";
-import Header from "./components/header/Header";
+import React, { useState } from "react";
+import { CommentList } from "./components/comments/CommentList";
+// import Header from "./components/header/Header";
+import { Hero } from "./components/hero/Hero";
+import { ItemDetail } from "./components/item_detail/ItemDetail";
+import { SideBarList } from "./components/sideBar/SideBarList";
+import { getVideoDetails, getVideos } from "./utils/utils";
 import "./styles/App.scss";
-import { Home } from "./pages/Home/Home";
-import { Upload } from "./pages/Upload/Upload";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+	const defaultId = "84e96018-4022-434e-80bf-000ce4cd12b8";
+	const [videoId, setVideoId] = useState(defaultId);
+	const [videos, setVideos] = useState(getVideos(videoId));
+	const [videoDetails, setVideoDetails] = useState(getVideoDetails(videoId));
+
+	const handleClick = (event, currentVideoId) => {
+		event.preventDefault();
+		setVideoId(currentVideoId);
+		setVideos(getVideos(currentVideoId));
+		setVideoDetails(getVideoDetails(currentVideoId));
+	};
+
 	return (
 		<>
-			<BrowserRouter>
-				<Header />
-				<Routes>
-					<Route
-						path="/"
-						element={<Home />}
+			<Hero videoImg={videoDetails} />
+			<div className="app__container">
+				<div className="app__left-container">
+					<ItemDetail videoDetail={videoDetails} />
+					<CommentList videoComments={videoDetails.comments} />
+				</div>
+				<div className="app__right-container">
+					<SideBarList
+						videos={videos}
+						onVideoClick={handleClick}
 					/>
-					<Route
-						path="/upload"
-						element={<Upload />}
-					/>
-				</Routes>
-			</BrowserRouter>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Header from "./components/header/Header";
+import { Upload } from "./pages/Upload/Upload";
 import "./styles/App.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<BrowserRouter>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={<App />}
+				/>
+				<Route
+					path="/upload"
+					element={<Upload />}
+				/>
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
