92624c8a362defd6ae0a8fb013072d3167dd698d
NM <nanayamiki@gmail.com>
2022-11-12 23:40:28 -0800

    deleted comments

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import "./hero.scss";
-import Play from "../../assets/icons/play.svg";
-import Scrubber from "../../assets/icons/scrub.svg";
-import Close from "../../assets/icons/close_fullscreen.svg";
-import VolumeUp from "../../assets/icons/volume_up.svg";
 
 export const Hero = ({ videoImg }) => {
 	return (
@@ -19,26 +15,6 @@ export const Hero = ({ videoImg }) => {
 					type="video/mp4"
 				></source>
 			</video>
-			{/* <img
-				className="hero__play"
-				src={Play}
-				alt="play"
-			/>
-			<img
-				className="hero__scrub"
-				src={Scrubber}
-				alt="scrub"
-			/>
-			<img
-				className="hero__close"
-				src={Close}
-				alt="close_fullscreen"
-			/>
-			<img
-				className="hero__volumeup"
-				src={VolumeUp}
-				alt="volume_up"
-			/> */}
 		</div>
 	);
 };
